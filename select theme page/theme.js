7faedeee4a507f76f1e820a4d3c528a94dd55be5
Vinny <1998vss@gmail.com>
2021-12-30 17:51:10 -0300

    inserção de páginas da categoria animais

---
+++ select theme page/theme.js
@@ -0,0 +1,8 @@
+let escolha = ""
+let pressedButton = false
+function pressedButton() {
+    document.getElementsByClassName("imageButton")
+}
+
+// ao clicar na imagem, executar função
+// trocar valor da variável booleana 
