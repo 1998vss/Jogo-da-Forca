8bff122db2eaf278c45116d35595380ce48c3687
Vinny <1998vss@gmail.com>
2022-01-03 18:35:57 -0300

    .

--- select theme page/theme.js
+++ select theme page/theme.js
@@ -1,8 +1,3 @@
-let escolha = ""
-let pressedButton = false
-function pressedButton() {
-    document.getElementsByClassName("imageButton")
-}
 
 // ao clicar na imagem, executar função
 // trocar valor da variável booleana 
