bd159ae4f1b68aa7346a887ddae32a574f79aa16
Vinny <1998vss@gmail.com>
2022-01-04 19:37:10 -0300

    inserção do Javascript para seleção do tema

--- select theme page/theme.js
+++ select theme page/theme.js
@@ -1,3 +1,23 @@
+function keepSelected() {
+    document.getElementById("animals").style.border = "2px solid #B70A0A";
+    document.getElementById("work").style.border = "transparent";
+    document.getElementById("bed").style.border = "transparent";
+}
 
+function keepSelected2() {
+    document.getElementById("work").style.border = "2px solid #000000";
+    document.getElementById("animals").style.border = "transparent";
+    document.getElementById("bed").style.border = "transparent";
+}
+
+function keepSelected3() {
+    document.getElementById("bed").style.border = "2px solid #ffffff";
+    document.getElementById("animals").style.border = "transparent";
+    document.getElementById("work").style.border = "transparent";
+}
+
+function GoToPage() {
+    
+}
 // ao clicar na imagem, executar função
 // trocar valor da variável booleana 
