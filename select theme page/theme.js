9af565a908fb88353a405213ae861b843e516b28
Vinny <1998vss@gmail.com>
2022-01-04 20:51:50 -0300

    menu de escolha implementado com js

--- select theme page/theme.js
+++ select theme page/theme.js
@@ -1,23 +1,41 @@
+var animals = false
+var work = false
+var bed = false
+
 function keepSelected() {
     document.getElementById("animals").style.border = "2px solid #B70A0A";
     document.getElementById("work").style.border = "transparent";
     document.getElementById("bed").style.border = "transparent";
+    animals = true
 }
 
 function keepSelected2() {
     document.getElementById("work").style.border = "2px solid #000000";
     document.getElementById("animals").style.border = "transparent";
     document.getElementById("bed").style.border = "transparent";
+    work = true
 }
 
 function keepSelected3() {
     document.getElementById("bed").style.border = "2px solid #ffffff";
     document.getElementById("animals").style.border = "transparent";
     document.getElementById("work").style.border = "transparent";
+    bed = true
 }
 
-function GoToPage() {
-    
+function GoToPage(){
+    if (animals) {
+        console.log("animals")
+        location.href = "/Animals page/animals.html"
+    }
+    else if (work) {
+        console.log("work")
+        location.href = "/error.html"
+    }
+    else if (bed) {
+        console.log("bed")
+        location.href = "/error.html"
+    }
 }
 // ao clicar na imagem, executar função
 // trocar valor da variável booleana 
