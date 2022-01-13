39b002725d0e0ae651ea3fda372f3d3b8d8deb89
Vinny <1998vss@gmail.com>
2022-01-13 19:37:24 -0300

    .

--- Animals page/animals.js
+++ Animals page/animals.js
@@ -1,15 +1,15 @@
 var animals = ["dog", "cat", "hippo", "rhino", "zebra", "elephant"]
+let rng = Math.floor()*animals.length;
+let chosenWord = animals[rng];
 
 window.onload =  function startGame() {
-    for (let i = 0; i < animals[6].length; i++) {
+    for (let i = 0; i < animals[rng].length; i++) {
         let wordContainer = `<span id="${"letter-"+i}" class="letterContainer"></span>`;
         document.getElementById("divTexto").innerHTML += wordContainer;
         console.log(wordContainer);
     }
 }
 
-function letterClick() {
-    if () {
-        document.getElementById("letter-"+i).innerHTML = 
-    }
+function letterClick(letter) {
+   // if chosenWord.includes(letter)
 }
